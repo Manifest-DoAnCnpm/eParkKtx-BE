ae4f0980b4c3ac12abf37e2ddf972548acdf15d3
NghiaTran <nghiatran912005@gmail.com>
2025-10-14 13:53:35 +0700

    add userrepository

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
 package eparkktx
+
+import (
+	"github.com/gin-gonic/gin"
+	"eParkKtx/config"
+)
+
+
+func main(){
+	server := gin.Default();
+
+	config.ConnectDatabase()
+
+	
+
+}
--- repositories/userRepo.go
+++ repositories/userRepo.go
@@ -1,1 +1,30 @@
 package repositories
+
+
+import (
+    "eParkKtx/entities"
+	"eParkKtx/config"
+    "gorm.io/gorm"
+)
+
+type UserRepository struct {
+    DB *gorm.DB
+}
+
+func NewUserRepository() *UserRepository {
+    return &UserRepository{config.DB} // create user repository 
+}
+
+func (repo *UserRepository) CreateNewUser(user *entities.User) error {
+    return repo.DB.Create(user).Error // tạo user trong db
+}
+
+
+func (r *UserRepository) GetByName(Name string) (*entities.User, error) {
+    var user entities.User
+
+    if err := r.DB.First(&user, "Name = ?", Name).Error; err != nil { // if <khai báo biến>; <condition>{}
+        return nil, err
+    }
+    return &user, nil // trả về con trỏ tới user vừa tạo và nil -> ko lỗi
+}
