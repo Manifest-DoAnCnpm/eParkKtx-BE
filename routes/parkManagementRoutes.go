546dbcea0021e37461f9e4bdd7c7d36548dbe246
Nghia Tran <nghiatran912005@gmail.com>
2025-12-02 10:48:01 +0700

    fix git

--- routes/parkManagementRoutes.go
+++ routes/parkManagementRoutes.go
@@ -2,13 +2,12 @@ package routes
 
 import (
 	"eParkKtx/controllers"
-	"eParkKtx/services"
 
 	"github.com/gin-gonic/gin"
 )
 
 // SetupParkManagementRoutes sets up the routes for park management
-func SetupParkManagementRoutes(r *gin.Engine, parkManagementController *controllers.ParkManagementController, userService *services.UserService) {
+func SetupParkManagementRoutes(r *gin.Engine, parkManagementController *controllers.ParkManagementController) {
 	// Group park management routes under /api/park-management
 	parkManagementGroup := r.Group("/api/park-management")
 	{
